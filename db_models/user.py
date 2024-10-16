5c74407e65422a54dc5ef14d270cc132327459cc
MesudeTurkmen <mesudeturkmen0@google.com>
2024-10-16 14:04:45 +0300

    Update user.py

--- db_models/user.py
+++ db_models/user.py
@@ -9,6 +9,27 @@ class User(TimeStampedModel):
     nick_name = Column(String(80), nullable=False)
     password = Column(String(80), nullable=False)
 
+    roles = Relationship('Role', secondary='user_roles',back_populates='users', passive_deletes=True)
+
     def __repr__(self):
         return f'{self.__class__.__name__},name: {self.first_name} {self.last_name}'
     
+class Role(Model):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(80),nullable=False)
+    slug = Column(String(80),nullable=False, unique=True)
+
+    users = Relationship('User', secondary='user_roles', back_populates='roles', passive_deletes=True)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}, name:{self.name}'
+    
+
+
+class UserRole(TimeStampedModel):
+    __tablename__ = 'user_roles'
+
+    user_id = Column(Integer, ForeignKey('users.id', ondelete=' CASCADE'), primary_key=True)
+    role_id = Column(Integer, ForeignKey('roles.id', ondelete=' CASCADE'), primary_key=True)
