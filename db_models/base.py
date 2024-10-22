3ecfb2b615fa95e2cc6fe231b5988c815a5f47e3
MesudeTurkmen <mesudeturkmen0@google.com>
2024-10-22 21:04:01 +0300

    db denemeleri

--- db_models/base.py
+++ db_models/base.py
@@ -1,14 +1,3 @@
-from sqlalchemy.orm import declarative_base
-from main import session
-from sqlalchemy import Column, DateTime
-from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
 
-Model = declarative_base
-Model.query = session.query_property()
-
-class TimeStampedModel(Model):
-    __abstract__ = True
-
-    created_at = Column(DateTime, default=datetime.utcnow())
-    updated_at = Column(DateTime, onupdate=datetime.utcnow())
-    
+Base = declarative_base()
--- db_models/user.py
+++ db_models/user.py
@@ -1,35 +1,12 @@
-from db_models.base import Model, TimeStampedModel
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import Relationship 
+from sqlalchemy import Column, Integer, String
+from db_models.base import Base
 
-class User(TimeStampedModel):
+class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    nick_name = Column(String(80), nullable=False)
-    password = Column(String(80), nullable=False)
-
-    roles = Relationship('Role', secondary='user_roles',back_populates='users', passive_deletes=True)
+    id = Column(Integer, primary_key=True)
+    nickname = Column(String)
+    password = Column(String)
 
     def __repr__(self):
-        return f'{self.__class__.__name__},name: {self.first_name} {self.last_name}'
-    
-class Role(Model):
-    __tablename__ = 'roles'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(80),nullable=False)
-    slug = Column(String(80),nullable=False, unique=True)
-
-    users = Relationship('User', secondary='user_roles', back_populates='roles', passive_deletes=True)
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}, name:{self.name}'
-    
-
-
-class UserRole(TimeStampedModel):
-    __tablename__ = 'user_roles'
-
-    user_id = Column(Integer, ForeignKey('users.id', ondelete=' CASCADE'), primary_key=True)
-    role_id = Column(Integer, ForeignKey('roles.id', ondelete=' CASCADE'), primary_key=True)
+        return f"<User(name='{self.nickname}', email='{self.password}')>"
