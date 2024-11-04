d28befe1dc589a47440120e74008d348aea7aac5
MesudeTurkmen <mesudeturkmen0@google.com>
2024-11-05 02:11:46 +0300

    asyncio

--- asyncio/server.py
+++ asyncio/server.py
@@ -1,4 +1,5 @@
 import asyncio
+from db import User
 
 class Server:
     def __init__(self, host='0.0.0.0', port=6666):
@@ -18,37 +19,35 @@ class Server:
         addr = writer.get_extra_info('peername')
         print(f"Connection established with {addr}")
         self.clients.append(writer)
-    
-        # Kullanıcının nickname'ini isteme
-        writer.write(b"Enter your nickname: ")
-        await writer.drain()
-        nickname = (await reader.read(100)).decode().strip()
+
+       
+        nickname = User.nickname
         print(f"{nickname} has joined the chat.")
-    
+
         try:
             while True:
                 data = await reader.read(100)
-                
+
                 if not data:
                     print(f"Connection closed by {addr}")
                     break
-                
+
                 message = data.decode()
                 print(f"{nickname}: {message}")
-    
+
                 # Mesajı diğer kullanıcılara yayma
                 broadcast_message = f"{nickname}: {message}".encode()
                 await asyncio.gather(*(self.send_to_client(client, broadcast_message) for client in self.clients if client != writer))
-    
+
         except Exception as e:
             print(f"Error with client {addr}: {e}")
-        
+
         finally:
             print(f"Closing connection with {addr}")
             writer.close()
             await writer.wait_closed()
             self.clients.remove(writer)
-    
+
     async def send_to_client(self, client, data):
         client.write(data)
         await client.drain()
